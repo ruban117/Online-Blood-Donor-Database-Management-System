667e5595a9529eef76ba1787f91dc975fa0116de
Ruban <rubanpathak706@gmail.com>
2023-09-16 22:13:35 +0530

    Modified Donor Login Form And Donor Signup Form

--- BLOOD DONOR LOGIN FORM/Donor_LoginForm.php
+++ BLOOD DONOR LOGIN FORM/Donor_LoginForm.php
@@ -19,7 +19,7 @@
   
   <div class="inner">
     <div class="heading">
-      <h3>Join As Donors</h3>
+      <h3>Login As Donors</h3>
     </div>
   </div>
   <div class="inner2">
@@ -38,11 +38,46 @@
 
           <button type="submit" class="btn btn-primary text-center" name="sub">Submit</button>
         </form>
-        <p id="new">New User? <span style="color:#fe0000;">Sign Up</span></p>
+        <p class="forget" data-toggle="modal" data-target="#myModal">Forget Password?</p>
+        <p id="new">New User? <a href="Donor_SignUPForm.php" style="color:#fe0000;">Sign Up</a></p>
+      </div>
+    </div>
+  </div>
+  <!-- Modal -->
+  <div class="modal fade" id="myModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h1 class="modal-title fs-5" id="exampleModalLabel">Change The Password</h1>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          <form>
+            <div class="mb-3">
+              <label for="exampleInputEmail1" class="form-label">Email address</label>
+              <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+            </div>
+            <div class="mb-3">
+              <label for="exampleInputPassword1" class="form-label">Change Password</label>
+              <input type="password" class="form-control" id="exampleInputPassword1">
+            </div>
+          </form>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+          <button type="button" class="btn btn-primary">Save changes</button>
+        </div>
       </div>
     </div>
   </div>
   <?php include "Assests/_footer.php" ?>
+  <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+  <script>
+    // jQuery to handle the form submission and show the modal
+    document.getElementsByTagName('p')[0].addEventListener('click',(e)=>{
+      $("#myModal").modal("show");
+    })
+  </script>
 </body>
 
 </html>
